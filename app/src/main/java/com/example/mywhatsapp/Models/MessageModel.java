6ca5c0146b4a1379d9b4fe1b64dbfa850e7e2cfb
pnkj51 <pankaj.shivharecs2018@indoreinstitute.com>
2021-07-25 18:54:45 +0530

    MessageModel Added

---
+++ app/src/main/java/com/example/mywhatsapp/Models/MessageModel.java
@@ -0,0 +1,44 @@
+package com.example.mywhatsapp.Models;
+
+public class MessageModel {
+    String uId,msg;
+    Long timeStamp;
+
+    public MessageModel(String uId, String msg, Long timeStamp) {
+        this.uId = uId;
+        this.msg = msg;
+        this.timeStamp = timeStamp;
+    }
+
+    public MessageModel(String uId, String msg) {
+        this.uId = uId;
+        this.msg = msg;
+    }
+
+    public MessageModel() {
+    }
+
+    public String getuId() {
+        return uId;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public Long getTimeStamp() {
+        return timeStamp;
+    }
+
+    public void setuId(String uId) {
+        this.uId = uId;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    public void setTimeStamp(Long timeStamp) {
+        this.timeStamp = timeStamp;
+    }
+}
